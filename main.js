9382181f10d51e247a64165b6f03394ac433a840
Alessandro M <56905412+Alessandro-Maninchedda@users.noreply.github.com>
2020-06-13 09:28:50 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,46 +1,82 @@
-$(document).ready(function(){
-   var subtotal = 0;
-   var ultimo_operador = "";
-   $(".numero").click(function(){
-     var numero = $(".total-calcu").val() + $(this).val();
-     $(".total-calcu").val(numero);
-   });
-   $(".operador").click(function(){
-     var numero = $(".total-calcu").val(); 
-     var operador = $(this).val(); 
-     ultimo_operador = operador;
-     if(numero != ""){
-       if(ultimo_operador == "+"){
-         subtotal = parseInt(subtotal) + parseInt(numero);
-       }else if(ultimo_operador == "-"){
-         subtotal = parseInt(subtotal) - parseInt(numero);
-       }
-       else if(ultimo_operador == "/"){
-         subtotal = parseInt(subtotal) / parseInt(numero);
-       }else{
-         subtotal = parseInt(subtotal) * parseInt(numero);
-       }
-       alert(subtotal);
-       $(".total-calcu").val(''); 
-       $(".sub-total").val(
-               $(".sub-total").val()  + numero + operador
-       );
-     }
-   });
-   $(".total-operacion").click(function(){
-     var numero = $(".total-calcu").val();
-     if(numero != ""){
-       if(ultimo_operador == "+"){
-         subtotal = parseInt(subtotal) + parseInt(numero);
-       }else if(ultimo_operador == "-"){
-         subtotal = parseInt(subtotal) - parseInt(numero);
-       }
-       else if(ultimo_operador == "/"){
-         subtotal = parseInt(subtotal) / parseInt(numero);
-       }else{
-         subtotal = parseInt(subtotal) * parseInt(numero);
-       }
-     }
-     $(".total-calcu").val(subtotal);
-   });
- });
+function getHistorial(){
+	return document.getElementById("valor-historial").innerText
+}
+
+function printHistorial(numero){
+	document.getElementById("valor-historial").innerText = numero
+}
+
+function getTotal(){
+	return document.getElementById("total-valor").innerText
+}
+
+function printTotal(numero){
+	if(numero == ""){
+		document.getElementById("total-valor").innerText = numero
+	}
+	else{
+		document.getElementById("total-valor").innerText = getFormattedNumber(numero)
+	}	
+}
+
+function getFormattedNumber(numero){
+	if(numero == "-"){
+		return ""
+	}
+	var num = Number(numero)
+	var valor = num.toLocaleString("en")
+	return valor
+}
+
+function reverseNumberFormat(numero){
+	return Number(numero.replace(/,/g,''))
+}
+
+var operator = document.getElementsByClassName("operador")
+for(var i = 0; i < operator.length; i++){
+	operator[i].addEventListener('click',function(){
+		if(this.id=="C"){
+			printHistorial("")
+			printTotal("")
+		} else if(this.id == "CE"){
+			var total=reverseNumberFormat(getTotal()).toString();
+			if(total){//if total tiene un valor
+				total = total.substr(0,total.length-1)
+				printTotal(total)
+			}
+		} else{
+			var total = getTotal();
+			var historial = getHistorial()
+			if(total == "" && historial != ""){
+				if(isNaN(historial[historial.length - 1])){
+					historial = historial.substr(0,historial.length-1)
+				}
+			}
+			if(total != "" || historial != ""){
+				total = total == "" ? total:reverseNumberFormat(total)
+				historial = historial + total
+				if(this.id == "="){
+					var result = eval(historial)
+					printTotal(result)
+					printHistorial("")
+				} else{
+					historial = historial + this.id
+					printHistorial(historial)
+					printTotal("")
+				}
+			}
+		}
+		
+	});
+}
+
+var numero = document.getElementsByClassName("numero")
+for(var i = 0; i < numero.length; i++){
+	numero[i].addEventListener('click',function(){
+		var total = reverseNumberFormat(getTotal())
+		if(total != NaN){ //if total es un numero
+			total = total + this.id
+			printTotal(total)
+		}
+	});
+}
